d18473c2e12511268857963e8c431f74a0e7a908
Alexander Michael Roche <alex@amroche.co.uk>
2013-03-28 23:20:05 +0000

    Added playing logic and actually playing stuff now. Now for the sound bit.

--- mapping/script/audioSettings.js
+++ mapping/script/audioSettings.js
@@ -15,7 +15,7 @@ var graphResolution = 100; //period in seconds to display on the graph.
 // onChange = (function(){setPlayArray(1,"+feedId+","+dataId+",this.value)});
 //	JSON[feedId] array structure for each object.[dataId, currentVal, maxVal, minVal, GRAPH, sonificationInformation[] ]);
 // sonificationInformation[] structure : 
-
+//http://freesound.org/people/fins/sounds/?page=1#sound
 setInterval(function() {
 	checkForSongData();
 }, 60000/BPM);
@@ -41,33 +41,40 @@ for(var key in JSON){
 function checkForSongData(){
 	console.log(JSON);
 	//DO ALL OF THE PLAYCHECKING HERE BITCHES!!!!
+	for(var key in JSON){
+	for(var i = 0; i < JSON[key].length; i++)
+		{
 						//"Greater>> Less<< Changes from Original=|="
-					/*	var tester = JSON[json[i].id][IJSON][5];
+						var tester = JSON[key][i][5];
 						if(tester[0] != undefined && tester[1]!=undefined&&tester[2]!=undefined && tester[3]==true){
 							//0 is mode, 1 is threshold, 2 is sound //checks values set in audioSettings.js
+							console.log("allDef'd");
+							console.log(tester);
 							if(tester[0] == "=|="){
-								if(tester[1]!=json[i].datastreams[dataI].current_value){
-									playNoise(tester[1]);
+								if(tester[1]!=JSON[key][i][1]){
+									playNoise(tester[2]);
 								}
 							}
 							else if(tester[0] == ">>"){
-								if(tester[1]>json[i].datastreams[dataI].current_value){
-									playNoise(tester[1]);
+								if(tester[1]>JSON[key][i][1]){
+									playNoise(tester[2]);
 								}
 							}
 							else if(tester[0] == "<<"){
-								if(tester[1]<json[i].datastreams[dataI].current_value){
-									playNoise(tester[1]);
+								if(tester[1]<JSON[key][i][1]){
+									playNoise(tester[2]);
 								}
 							}
 							else if(tester[0] == "==" || tester[0] == "|==|"){
-								if(tester[1]==json[i].datastreams[dataI].current_value){
-									playNoise(tester[1]);
+								if(tester[1]==JSON[key][i][1]){
+									playNoise(tester[2]);
 								}
 							}							
 						}
-						*/
+						
 	
+}
+}
 }
 function getType(input) {
     var m = (/[\d]+(\.[\d]+)?/).exec(input);
@@ -118,6 +125,6 @@ function getType(input) {
  }
  
  function playNoise(songId){
- 	
+ 	console.log(songId);
  	
  }
